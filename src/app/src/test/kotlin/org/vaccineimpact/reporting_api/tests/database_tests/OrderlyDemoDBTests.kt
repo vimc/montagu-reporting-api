84c0c8bf3effb5cf1035059e9f258d3c6abb734d
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-05-03 15:58:37 +0100

    Fix tests for orderly multidb schema

--- src/app/src/test/kotlin/org/vaccineimpact/reporting_api/tests/database_tests/OrderlyDemoDBTests.kt
+++ src/app/src/test/kotlin/org/vaccineimpact/reporting_api/tests/database_tests/OrderlyDemoDBTests.kt
@@ -331,7 +331,8 @@ class OrderlyDemoDBTests : DatabaseTests()
                 assertThat(r.id).isGreaterThan(0)
                 assertThat(r.reportVersion).isNotBlank()
                 assertThat(r.name).isNotBlank()
-                assertThat(r.sql).isNotBlank()
+                assertThat(r.query).isNotBlank()
+                assertThat(r.database).isNotBlank()
                 assertThat(r.hash).isNotBlank()
             }
 
